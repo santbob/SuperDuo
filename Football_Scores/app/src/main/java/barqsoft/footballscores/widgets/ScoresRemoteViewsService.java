6f72fb126efebc40e146dc05fd81ced330a9fe76
Santhosh Nageshwar <santhoshbabun@gmail.com>
2016-01-10 15:07:33 -0800

    Making the app support supports layout mirroring

---
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/widgets/ScoresRemoteViewsService.java
@@ -0,0 +1,14 @@
+package barqsoft.footballscores.widgets;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by santhosh on 10/01/16.
+ */
+public class ScoresRemoteViewsService extends IntentService {
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
